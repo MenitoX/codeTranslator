7c4821878e5d91892d8032108ff82f78fe6f5806
Menitox <pablocontrerasochoa@gmail.com>
2021-04-30 07:58:15 -0400

    refactor(package):  commentaries ;)

--- package/codeBase.py
+++ package/codeBase.py
@@ -29,16 +29,27 @@ def binaryToBcd(binary):
         
     return decimalValue
 
+# Esta wea de Bcd no puede estar peor explicada porque directamente no funciona
+# porque el ejemplo está mal por la cresta supuestamente es pasar de binario a dec y de ese
+# dec a BCD cada digito, pero el ejemplo hace algo nada que ver,  WNNN COMO NO PUEDEN DAR BIEN INSTRUCCIONES DE ALGO QUE YO MISMO
+# ENCONTRÉ EN 5 MINUTOS DE GOOGLEAR PO LOCO WTF SAQUENME DE LATINOAMERICA CTM
 def numberToBcd(number, base):
     if base != __DEC_BASE__:
         number = baseToDecimal(number, base)
     number = list(str(number))
     number = [str(decimalToBase(i , __BIN_BASE__)) for i in number]
+    
+    # This block of code gives us the binary BCD 
+    
     for i in range(len(number)):
-        while len(number[i]) < 4:
-            number[i] = '0' + number[i]
+       while len(number[i]) < 4:
+           number[i] = '0' + number[i]
+    
+    
     number = "".join(number)
+    # SUPONGO HACER ESTO PQ SINO NO TIENE SENTIDO ESTA WEEAAAAA
     return number
+    #return baseToDecimal(number ,__BIN_BASE__)
     
 
 def bcdToNumber(number, base):
