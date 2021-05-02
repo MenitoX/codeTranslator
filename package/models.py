4db8ca81e729cd0dca056c300a5cb39c5ddfd898
Menitox <pablocontrerasochoa@gmail.com>
2021-05-02 02:17:35 -0400

    feat(package): codeArray no in models export

--- package/models.py
+++ package/models.py
@@ -1,14 +1,11 @@
 #Exports
-__all__ = ['responseChar', 'codeArray', 'binArray', 'digitArray', '__BIN_BASE__', '__DEC_BASE__','__START_POS__', '__MIN_BASE__', '__MAX_BASE__', '__MAX_N__', '__MIN_N__']
+__all__ = ['responseChar', 'codeArray', 'digitArray', '__BIN_BASE__', '__DEC_BASE__','__START_POS__', '__MIN_BASE__', '__MAX_BASE__', '__MAX_N__', '__MIN_N__']
 
 # Response chars array
 responseChar = list("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+?")
 
 # Possible codes array
 codeArray = ["bcd", "gry", "ed3", "jsn", "par", "pbt", "ham"]
-
-# Binary numbers array
-binArray = ["0", "1"]
 
 # Digits Array
 digitArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
